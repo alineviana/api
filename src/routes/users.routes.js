d857fd84f37b8c6b565388929d7b227be9ef4975
Aline Cristina Viana da Costa <alinecristinaviana@gmail.com>
2023-02-22 14:04:41 -0300

    feat: upload images

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,13 +1,20 @@
 const { Router } = require("express");
+const multer = require("multer");
+const uploadConfig = require("../configs/upload");
 
 const usersRoutes = Router();
+const upload = multer(uploadConfig.MULTER);
 
 const UsersController = require("../controllers/UsersController");
 const usersController = new UsersController();
+
+const UserAvatarController = require("../controllers/UserAvatarController");
+const userAvatarController = new UserAvatarController();
 
 const ensureAuthenticaded = require("../middlewares/ensureAuthenticaded");
 
 usersRoutes.post("/", usersController.create);
 usersRoutes.put("/", ensureAuthenticaded, usersController.update);
+usersRoutes.patch("/avatar", ensureAuthenticaded, upload.single("avatar"), userAvatarController.update);
 
 module.exports = usersRoutes;
